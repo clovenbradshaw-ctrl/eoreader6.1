79a030df74c0ddf38d23f7e67af619c763a95c99
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-21 15:46:23 -0500

    Export EOT reasoning surface

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -4,3 +4,6 @@ export { judge } from "./search/index.js";
 export {
   createArrivalTracker, trackArrival, beliefConstellation, castBelief, arrivalReading,
 } from "./tracking/arrival.js";
+export {
+  normalizeEotTuple, buildEotGraph, reasonOverEot, renderEotReasoning,
+} from "./reasoning/eot.js";
