2493121e7f9d09e29ab7c7cae784f20cd55b48d2
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-22 21:52:26 -0500

    Export fold-conditioned recursive reading APIs

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -10,3 +10,13 @@ export {
 export {
   falsificationEnvelope, falsificationEnvelopes, renderFalsificationEnvelope,
 } from "./reasoning/falsification.js";
+
+export { receivedGround, eoOperation, deltaFold, applyObservation, applyDelta, reconstruct } from "./fold/index.js";
+export { deriveOrientation } from "./orientation/index.js";
+export { perceive } from "./perception/index.js";
+export { witness } from "./witness/index.js";
+export { addressOf, cubeAddresses, relevantNeighborhood, interrogateCube, deriveEOTransformations } from "./reasoning/fold-conditioned.js";
+export { expectation, expectationTransition, EXPECTATION_STATES } from "./expectations/index.js";
+export { obligation, openObligation, resolveObligation, carryObligations } from "./obligations/index.js";
+export { deriveSurprise, deriveTension, deriveRelease } from "./dynamics/index.js";
+export { encounter, createRecursiveReader } from "./reading/recursive.js";
