1dbb89a23f36cdba0eb0d232650250a849f40d18
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-22 22:36:30 -0500

    Export structural pattern and graph revision APIs

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -11,12 +11,14 @@ export {
   falsificationEnvelope, falsificationEnvelopes, renderFalsificationEnvelope,
 } from "./reasoning/falsification.js";
 export { hyperedge, graphObject, buildHypergraph, relevantHypergraphNeighborhood } from "./hypergraph/index.js";
+export { discoverPatternCandidates } from "./patterns/index.js";
 
 export { receivedGround, eoOperation, deltaFold, applyObservation, applyDelta, reconstruct } from "./fold/index.js";
 export { deriveOrientation } from "./orientation/index.js";
 export { perceive } from "./perception/index.js";
 export { witness } from "./witness/index.js";
 export { addressOf, cubeAddresses, relevantNeighborhood, interrogateCube, deriveEOTransformations } from "./reasoning/fold-conditioned.js";
+export { deriveGraphStructuralDelta } from "./reasoning/graph-structural.js";
 export { expectation, expectationTransition, EXPECTATION_STATES } from "./expectations/index.js";
 export { obligation, openObligation, resolveObligation, carryObligations } from "./obligations/index.js";
 export { deriveSurprise, deriveTension, deriveRelease } from "./dynamics/index.js";
