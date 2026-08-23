65ac7abe1778f779e5d7b36a83e3786cf1bdf33b
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-08-22 22:46:24 -0500

    Export reading task scheduler

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -23,7 +23,8 @@ export { expectation, expectationTransition, EXPECTATION_STATES } from "./expect
 export { obligation, openObligation, resolveObligation, carryObligations } from "./obligations/index.js";
 export { deriveSurprise, deriveTension, deriveRelease } from "./dynamics/index.js";
 export {
-  createReadingTaskState, taskForObligation, proposeObligationTasks, reconcileObligationTasks,
+  createReadingTaskState, taskForObligation, taskPriority, scheduleTasks,
+  proposeObligationTasks, reconcileObligationTasks,
   wakeTasks, executeClarificationTask, appendTaskResult, typeTask,
 } from "./tasks/reading.js";
 export { encounter, createRecursiveReader } from "./reading/recursive.js";
